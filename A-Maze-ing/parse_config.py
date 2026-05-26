80db3907c5713522e13c7d3457c3e2f12e2bc88b
Daniel Hontanilla Diaz <dhontani@c2r4s2.42madrid.com>
2026-05-26 18:55:59 +0200

    f

--- A-Maze-ing/parse_config.py
+++ A-Maze-ing/parse_config.py
@@ -1,5 +1,7 @@
+import sys
+
 def parse_config(filepath: str) -> dict:
-	"""Parse the maze configuration file.
+    """Parse the maze configuration file.
 
     Args:
         filepath: Path to the configuration file.
@@ -9,15 +11,48 @@ def parse_config(filepath: str) -> dict:
 
     Raises:
         FileNotFoundError: If the file does not exist.
+        SyntaxError: If the syntax is not KEY=VALUE.
+    """
+    config = {}
+    try:
+        with open(filepath, "r") as f:
+            for line in f:
+                try:
+                    if line.startswith("#") or line == "\n" :
+                        continue
+                    else:
+                        if "=" not in line:
+                            raise SyntaxError("Bad syntax")
+                        else:
+                            key, value = line.split("=", 1)
+                            key = key.strip()
+                            value = value.strip()
+                            config[key] = value
+                except SyntaxError as e:
+                    print(e)
+                    return
+    except FileNotFoundError as er:
+        print(er)
+        return
+    return config
+
+
+def dict_validation(config: dict[str, str]) -> dict:
+    """Validate the dictionary elements from parse_config.
+
+    Args: 
+        config: Already parsed dictionary.
+    
+    Returns:
+        A dictionary ready for the generator to use.
+    
+    Raises:
         ValueError: If a required key is missing or has an invalid value.
     """
-	validated_dict: {}
-	with open(filepath, "r") as f:
-		for line in f:
-			if line.startswith("#") or line == "\n" :
-				continue
-			elif:
-				if "=" not in line:
-					raise SyntaxError("Bad syntax")
-				else:
-					line.split("=")
+    if config is None:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    configuration = print(parse_config(sys.argv[1]))
+    if configuration is not None:
+        print(dict_validation(configuration))
