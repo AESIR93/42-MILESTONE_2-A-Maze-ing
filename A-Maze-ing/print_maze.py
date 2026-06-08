90fdb7f81251da9e21d5c154782acb72eb5799d0
DaniPersonal <dhontani@student.42madrid.com>
2026-06-08 22:07:11 +0200

    render listo

---
+++ A-Maze-ing/print_maze.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+class Cell():
+    def __init__(self, north=True, south=True, west=True, east=True):
+        self.north = north
+        self.south = south
+        self.west = west
+        self.east = east
+
+def render(maze: list[list[Cell]]) -> None:
+    for row in maze:
+        line_top = ""
+        for cell in row:
+            line_top += "+"
+            if cell.north:
+                line_top += "--"
+            else:
+                line_top += "  "
+        line_top += "+"
+        print(line_top)
+        line_side = ""
+        for cell in row:
+            if cell.west:
+                line_side += "|  "
+            else:
+                line_side += "   "
+        line_side += "|"
+        print(line_side)
+    last_row = maze[-1]
+    line_bot = ""
+    for cell in last_row:
+        line_bot += "+--"
+    line_bot += "+"
+    print(line_bot)
+
+def main():
+    maze = [[Cell() for _ in range(8)] for _ in range(4)]
+    maze[1][1].north = False
+    maze[2][4].west = False
+    maze[0][3].north = False
+    maze[1][6].west = False
+    render(maze)
+
+if __name__ == "__main__":
+    main()
