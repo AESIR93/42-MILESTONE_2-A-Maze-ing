d3d4693b2c8414661a5e1e7b3ad418245e0e7813
Daniel Hontanilla Diaz <dhontani@c1r7s6.42madrid.com>
2026-07-17 18:44:34 +0200

    ahora si que si

--- Amazeing/print_maze.py
+++ Amazeing/print_maze.py
@@ -3,18 +3,22 @@
 from maze_generator import Cell
 import time
 
+
 def render_path(coords: list[tuple[int, int]], animate: bool = True) -> None:
     if animate is True:
         for x, y in coords[1:-1]:
-            fila = y * 2 + 2
+            fila = y * 2 + 1
             columna = x * 3 + 2
-            print(f"\033[{fila};{columna}H\033[1;93m*\033[0m", end="", flush=True)
+            print(f"\033[{fila};{columna}H"
+                  f"\033[1;93m*\033[0m",
+                  end="", flush=True)
             time.sleep(0.02)
     else:
         for x, y in coords[1:-1]:
-            fila = y * 2 + 2
+            fila = y * 2 + 1
             columna = x * 3 + 2
-            print(f"\033[{fila};{columna}H\033[1;93m*\033[0m", end="", flush=True)
+            print(f"\033[{fila};{columna}H\033[1;93m*\033[0m",
+                  end="", flush=True)
 
 
 def path_to_coords(path: list[str],
@@ -36,7 +40,7 @@ def path_to_coords(path: list[str],
 
 
 def render(maze: list[list[Cell]],
-           path_coords: list[tuple[int, int]] = None,
+           path_coords: list[tuple[int, int]],
            wall_color: str = "\033[1;95m") -> None:
     """Print the maze in the terminal.
 
@@ -61,16 +65,20 @@ def render(maze: list[list[Cell]],
                 line_side += "\033[1;92m║██\033[0m"
             elif cell.west:
                 if ((x, y) == path_coords[0]):
-                    line_side += f"{wall_color}║\033[1;92m▓\033[0m\033[1;38;5;22m»\033[0m"
+                    line_side += (f"{wall_color}║\033[1;92m▓\033[0m"
+                                  f"\033[1;38;5;22m»\033[0m")
                 elif (x, y) == path_coords[len(path_coords) - 1]:
-                    line_side += f"{wall_color}║\033[1;91m»\033[0m\033[1;38;5;202m▓\033[0m"
+                    line_side += (f"{wall_color}║\033[1;91m»\033[0m"
+                                  f"\033[1;38;5;202m▓\033[0m")
                 else:
                     line_side += f"{wall_color}║  \033[0m"
             else:
                 if ((x, y) == path_coords[0]):
-                    line_side += f"{wall_color}\033[1;92m▓\033[0m\033[1;38;5;22m»\033[0m "
+                    line_side += (f"{wall_color}\033[1; 92m▓\033[0m"
+                                  f"\033[1;38;5;22m»\033[0m ")
                 elif (x, y) == path_coords[len(path_coords) - 1]:
-                    line_side += f"{wall_color}\033[1;91m»\033[0m\033[1;38;5;202m▓\033[0m "
+                    line_side += (f"{wall_color}\033[1;91m»\033[0m"
+                                  f"\033[1;38;5;202m▓\033[0m ")
                 else:
                     line_side += "   "
         line_side += f"{wall_color}║\033[0m"
